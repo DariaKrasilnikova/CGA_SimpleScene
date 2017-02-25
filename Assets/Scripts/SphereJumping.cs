4a35e341fda6719d134c3817ffc5c855b9b28edb
Дарья Красильникова < >
2017-02-25 19:17:56 +0300

    Исправила bump на кубе

---
+++ Assets/Scripts/SphereJumping.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereJumping : MonoBehaviour {
+    public Vector3 movement = new Vector3(0.0f, 5.0f, 0.0f);
+
+    // Update is called once per frame
+    void Update () {
+        transform.Translate(movement * Mathf.Sin(Time.fixedTime));
+    }
+}
---
+++ Assets/Scripts/SphereMoving.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereMoving : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(Vector3.zero, Vector3.up, 60 * Time.deltaTime);
+    }
+}
